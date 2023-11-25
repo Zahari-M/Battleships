7a2c5567fd25577140ef0b590aad671771801d77
Zahari <zemarinov@gmail.com>
2023-11-25 12:31:09 +0200

    initial structure

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const express = require("express");
+const { engine } = require('express-handlebars');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => res.render('test'));
+
+app.listen(port, "localhost", () => console.log(
+    `Express started on http://localhost:${port}; ` +
+    `press Ctrl-C to terminate.`));
