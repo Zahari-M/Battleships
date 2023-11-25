eccd4f8fcf5debf50151e6a569e7fc0985155525
Zahari <zemarinov@gmail.com>
2023-11-25 13:11:15 +0200

    home page

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('public'))
 
-app.get('/', (req, res) => res.render('test'));
+app.get('/', (req, res) => res.render('home'));
 
 app.listen(port, "localhost", () => console.log(
     `Express started on http://localhost:${port}; ` +
